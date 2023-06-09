d2b1f7821c488c5dfe7ed7d8573bd0055cfa1870
WarHawk <76548110+Nithish-Krishna@users.noreply.github.com>
2023-06-09 16:31:04 +0530

    Update Dashboard.js

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -230,7 +230,7 @@ function Dashboard() {
             <div className="timer">
               <Countdown
                 ref={timerRef}
-                date={Date.now() + time * 1}
+                date={Date.now() + time * 0.5}
                 autoStart={false}
                 renderer={renderer}
                 onComplete={onComplete}
