f80330b590c2382fda65e318c6b0efcf08c379d6
WarHawk <76548110+Nithish-Krishna@users.noreply.github.com>
2023-06-09 16:05:10 +0530

    Update TimerClock.js

--- src/components/core/TimerClock.js
+++ src/components/core/TimerClock.js
@@ -3,7 +3,7 @@ import React from "react";
 const TimerClock = ({ hours, minutes, seconds }) => {
   return (
     <div className="timer-clock">
-      {minutes} : {seconds}
+      {seconds} : {minutes}
     </div>
   );
 };
